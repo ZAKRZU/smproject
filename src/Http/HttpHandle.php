cb0a4e4f592b0900e7c8ed6cb4e793acad18c51f
ZAKRZU <mznicko655@gmail.com>
2022-07-08 16:46:46 +0200

    Fixed typo, moved api secrets to env

--- src/Http/HttpHandle.php
+++ src/Http/HttpHandle.php
@@ -18,10 +18,10 @@ class HttpHandle
     public function fetchBrickLinkInventory()
     {
         $BricklinkApi = new BricklinkAPI([
-            'consumerKey' => '',
-            'consumerSecret' => '',
-            'tokenValue' => '',
-            'tokenSecrect' => ''
+            'consumerKey' => $_ENV['CONSUMER_KEY'],
+            'consumerSecret' => $_ENV['CONSUMER_SECRET'],
+            'tokenValue' => $_ENV['TOKEN_VALUE'],
+            'tokenSecret' => $_ENV['TOKEN_SECRET']
         ]);   
         $response = $BricklinkApi->request('GET', '/inventories')
                     ->execute()->getRawResponse();
